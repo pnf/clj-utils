7b7abc67c04d79a52c595990187b53a3f41ead65
Peter Fraenkel <pnf@podsnap.com>
2014-07-16 10:31:07 -0400

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-utils
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_utils/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-utils.core-test
+  (:require [clojure.test :refer :all]
+            [clj-utils.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
