023ba0ed3d9e842048d43b07d3bdb6c43ab5e963
Peter Fraenkel <pnf@podsnap.com>
2014-12-05 17:11:00 -0500

    removed encoding experiment

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.typed "0.2.72"]
+                 [prismatic/schema "0.3.3"]
                  [clj-time "0.7.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [com.taoensso/timbre "3.3.1"
